a4003caf9ae6a6aef93ec6f04acc80496eb640a8
amarnath <amarnathreddy456987@gmail.com>
2021-04-29 18:27:28 +0530

    changes in Cashregister

--- Cashregister.js
+++ Cashregister.js
@@ -43,6 +43,8 @@ const checkCashRegister = (price, cash, cid) => {
   }
 
 
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], 
+  /*console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], 
   ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], 
-  ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+  ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))*/
+
+  //Output for above test case is {status: "OPEN", change: [["QUARTER", 0.5]]}
